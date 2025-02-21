cdff8d05e10ac31c4af1bec16657bf0e1bbc2914
Tommy Mai <43077278+tmai-p@users.noreply.github.com>
2025-02-21 17:31:41 -0500

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </StrictMode>
 );
