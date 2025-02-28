3549bcd05e4f96f437a0e6546a80c73d3be427f0
Tommy Mai <43077278+tmai-p@users.noreply.github.com>
2025-02-28 15:11:15 -0500

    updates

--- src/pages/Service.tsx
+++ src/pages/Service.tsx
@@ -1,29 +1,41 @@
 function Service() {
   return (
     <div className="container">
-      <p>Service pages</p>
-      <p>...</p>
-      <p>...</p>
-      <p>...</p>
-      <p>...</p>
-      <p>...</p>
-      <p>...</p>
-      <p>...</p>
-      <p>...</p>
-      <p>Service pages</p>
-      <p>...</p>
-      <p>...</p>
-      <p>...</p>
-      <p>...</p>
-      <p>...</p>
-      <p>...</p>
-      <p>...</p>
-      <p>...</p>
-      <p>Service pages</p>
-      <p>...</p>
-      <p>...</p>
-      <p>...</p>
-      <p>...</p>
+      <br />
+      <p>
+        <h4>Service</h4>
+        <span>
+          Our <b>company does not provide service</b> for your appliances.
+          However, as a courtesy to our customers, we are happy to assist with
+          service-related inquiries. If your unit is currently under warranty,
+          we will help schedule service for you. However, if your unit is out of
+          service, we will direct you to our local service company.
+        </span>
+      </p>
+      <br />
+      <p>
+        <h5>Warranty Coverage</h5>
+        <span>
+          Most manufactures offer a one-year warranty from the delivery date.
+          The warranty will cover issues caused by manufacturer defects.
+          However, cosmetic damage is not covered under warranty. If there are
+          issues due to installation, the warranty will not apply, and the
+          customer will be responsible for the service charge. Please refer to
+          your product manual for detailed warranty coverage information.
+        </span>
+      </p>
+      <br />
+
+      <p>
+        <h5>Cosmetic Damage Upon arrival</h5>
+        <span>
+          You have <b>3 to 5 business</b> from the time of delivery to report
+          any cosmetic damage to your unit. We will take appropriate action to
+          exchange the unit for our customers. However, any damages reported
+          <b>after 5 business days</b> will not be eligible for exchange.
+        </span>
+      </p>
+      <br />
     </div>
   );
 }
